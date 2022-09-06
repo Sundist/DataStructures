2230cb33a80d53a86507eafcfee19a9cfcd2925b
sundisttt@gmail.com <sundisttt@gmail.com>
2022-09-06 20:14:05 +0300

    --LinkedList completed -- DoublyLinkedList implemented but not finished...

---
+++ src/list/DoublyLinnkedListInt.java
@@ -0,0 +1,40 @@
+package list;
+
+public class DoublyLinnkedListInt extends AbstractListInt{
+
+    private Node head;
+
+    DoublyLinnkedListInt(){
+
+    }
+
+    @Override
+    protected void init() {
+
+    }
+
+    @Override
+    public void add(int data) {
+
+    }
+
+    @Override
+    public boolean remove(int data) {
+        return false;
+    }
+
+    @Override
+    public int removeIndex(int index) {
+        return 0;
+    }
+
+    @Override
+    public int get(int index) {
+        return 0;
+    }
+
+    @Override
+    public int indexOf(int data) {
+        return 0;
+    }
+}
---
+++ src/list/Main.java
@@ -0,0 +1,17 @@
+package list;
+
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Linked List Main");
+        LinkedListInt list = new LinkedListInt();
+        int SIZE = 15;
+        Random random = new Random();
+        for (int i = 0; i < SIZE; i++) {
+            list.add(random.nextInt(10000));
+            System.out.print(list.get(i) + " ");
+        }
+    }
+
+}
