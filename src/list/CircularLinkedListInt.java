a609c0fbc50a674bee56ca833ee27905418408f5
Sundist <sundisttt@gmail.com>
2022-09-13 10:06:00 +0300

    Circular Linked List finished...

---
+++ src/list/CircularLinkedListInt.java
@@ -0,0 +1,78 @@
+package list;
+
+public class CircularLinkedListInt extends AbstractListInt {
+    private Node head;
+
+    public CircularLinkedListInt() {
+        init();
+    }
+
+    protected void init() {
+        head = null;
+    }
+
+    @Override
+    public void add(int data) {
+        Node node = new Node(data);
+        if (head != null) {
+            Node iter = head;
+            while (iter.next != head)
+                iter = iter.next;
+            node.next = head;
+            iter.next = node;
+        } else {
+            head = node;
+        }
+        size++;
+    }
+
+    @Override
+    public boolean remove(int data) {
+        if (head != null) {
+            Node iter = head;
+            while (iter.next.data != data) iter = iter.next;
+            iter.next = iter.next.next;
+            size--;
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public int removeIndex(int index) {
+        if (head != null) {
+            Node iter = head;
+            for (int i = 0; i < index - 1; i++) iter = iter.next;
+            iter.next = iter.next.next;
+            size--;
+            return 1;
+        }
+        return 0;
+    }
+
+    @Override
+    public int get(int index) {
+        if (head != null) {
+            Node iter = head;
+            for (int i = 0; i < index; i++)
+                iter = iter.next;
+            return iter.data;
+        }
+        return 0;
+    }
+
+    @Override
+    public int indexOf(int data) {
+        int counter = 0;
+        if (head != null) {
+            Node iter = head;
+            while (iter.data != data) {
+                counter++;
+                iter = iter.next;
+            }
+            return counter;
+        }
+        return -1;
+    }
+}
+
