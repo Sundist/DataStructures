b507319f17183ca82b000027e05a441c86ada4d7
Sundist <sundisttt@gmail.com>
2022-10-19 10:27:49 +0300

    Changes will be take place... Some modifications for Generic usage

---
+++ src/tree/AVLTree.java
@@ -0,0 +1,70 @@
+package tree;
+
+public class AVLTree implements TreeIntInterface {
+    @Override
+    public void add(int data) {
+
+    }
+
+    @Override
+    public boolean remove(int data) {
+        return false;
+    }
+
+    @Override
+    public void printInOrder() {
+
+    }
+
+    @Override
+    public void printPreOrder() {
+
+    }
+
+    @Override
+    public void printPostOrder() {
+
+    }
+
+    @Override
+    public boolean contains(int data) {
+        return false;
+    }
+
+    @Override
+    public int size() {
+        return 0;
+    }
+
+    @Override
+    public boolean isEmpty() {
+        return false;
+    }
+
+    @Override
+    public void clear() {
+
+    }
+
+    public void performLL() {
+
+    }
+
+    public void performLR() {
+
+    }
+
+    public void performRL() {
+
+    }
+
+    public void performRR() {
+
+    }
+
+    public boolean isBalanced() {
+        return false;
+    }
+
+
+}
